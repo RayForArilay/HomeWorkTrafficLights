5a48836cafa66b2472ae821cb5669a4ed1bc3313
RayForArilay <rayforarilay@gmail.com>
2025-06-22 10:15:45 +0300

    Done

--- HomeWorkTrafficLights/ViewController.swift
+++ HomeWorkTrafficLights/ViewController.swift
@@ -30,28 +30,25 @@ class ViewController: UIViewController {
         switcherButton.layer.cornerRadius = 30
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func clickButtonAction() {
-        if redLabel.alpha == 0.3 && greenLabel.alpha == 0.3 && yellowLabel.alpha == 0.3 {
-            redLabel.alpha = 0
-            switcherButton.setTitle("Next", for: .normal)
-        }
-        else if redLabel.alpha == 0 {
-            redLabel.alpha = 0.3
-            yellowLabel.alpha = 0
-            switcherButton.setTitle("Next", for: .normal)
+        if redLabel.alpha == 1 {
+                redLabel.alpha = 0.3
+                yellowLabel.alpha = 1
+                greenLabel.alpha = 0.3
             }
-        else if yellowLabel.alpha == 0 {
-            yellowLabel.alpha = 0.3
-            greenLabel.alpha = 0
+            else if yellowLabel.alpha == 1 {
+                yellowLabel.alpha = 0.3
+                greenLabel.alpha = 1
+                redLabel.alpha = 0.3
+            }
+            else {
+                greenLabel.alpha = 0.3
+                redLabel.alpha = 1
+                yellowLabel.alpha = 0.3
+            }
+            
             switcherButton.setTitle("Next", for: .normal)
-        }
-        else if greenLabel.alpha == 0 {
-            greenLabel.alpha = 0.3
-            redLabel.alpha = 0
-            switcherButton.setTitle("Next", for: .normal)
-        }
         }
     }
-    
 
