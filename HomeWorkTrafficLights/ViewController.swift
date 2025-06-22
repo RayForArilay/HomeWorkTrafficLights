2a1c36c8723bafdfef78b5128e219db2f16c6a0e
RayForArilay <rayforarilay@gmail.com>
2025-06-22 09:28:01 +0300

    Not working

--- HomeWorkTrafficLights/ViewController.swift
+++ HomeWorkTrafficLights/ViewController.swift
@@ -19,9 +19,39 @@ class ViewController: UIViewController {
         redLabel.alpha = 0.3
         greenLabel.alpha = 0.3
         yellowLabel.alpha = 0.3
+        
+        redLabel.layer.cornerRadius = redLabel.frame.width / 2
+        redLabel.layer.masksToBounds = true
+        greenLabel.layer.masksToBounds = true
+        yellowLabel.layer.masksToBounds = true
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.width / 2
+        
+        switcherButton.layer.cornerRadius = 30
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBAction func clickButtonAction() {
+        if redLabel.alpha == 0.3 && greenLabel.alpha == 0.3 && yellowLabel.alpha == 0.3 {
+            redLabel.alpha = 0
+            switcherButton.setTitle("Next", for: .normal)
+        }
+        else if redLabel.alpha == 0 {
+            redLabel.alpha = 0.3
+            yellowLabel.alpha = 0
+            switcherButton.setTitle("Next", for: .normal)
+            }
+        else if yellowLabel.alpha == 0 {
+            yellowLabel.alpha = 0.3
+            greenLabel.alpha = 0
+            switcherButton.setTitle("Next", for: .normal)
+        }
+        else if greenLabel.alpha == 0 {
+            greenLabel.alpha = 0.3
+            redLabel.alpha = 0
+            switcherButton.setTitle("Next", for: .normal)
+        }
+        }
+    }
+    
 
